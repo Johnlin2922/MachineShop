1249da778648412f39694302f22e1ba161f05c1c
Johnlin2922 <johnlin2922@gmail.com>
2017-11-02 19:33:58 -0500

    blah

--- src/main/java/edu/jsu/mcis/Main.java
+++ src/main/java/edu/jsu/mcis/Main.java
@@ -1,9 +1,26 @@
 package edu.jsu.mcis;
 
-public class Main {
+import java.io.*; 
+import java.util.*; 
 
+public class Main {
+	
+	private static MachineShop shop; 
 	public static void main(String[] args) {
 		
+		try{
+			Scanner s = new Scanner(new File("input.txt"));
+			shop = new MachineShop(s);
+			System.out.println("Time,Machine Queues, Active Jobs, Finished Times");
+			int counter = 0; 
+			shop.init(); 
+			while(!shop.allMachinesDone()){
+				System.out.println(Integer.toString(++counter) +  shop.toString());
+			}
+		}
+		catch (FileNotFoundException e){
+			e.getMessage();
+		}
 		
 		
 	}
